2f7f645c440ef976f448e4859c6622aef2c41d73
Ishidd <149577279+Ishidd@users.noreply.github.com>
2024-12-17 16:40:22 +0800

    Add files via upload

---
+++ dbo.GAMECHARACTERAPPEARANCE.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[GAMECHARACTERAPPEARANCE] (
+    [charID]        INT           IDENTITY (1, 1) NOT NULL,
+    [charSkinColor] NVARCHAR (50) NOT NULL,
+    [charHairStyle] NVARCHAR (50) NOT NULL,
+    [charHairColor] NVARCHAR (50) NOT NULL,
+    [charFaceShape] NVARCHAR (50) NOT NULL,
+    [charEyeShape]  NVARCHAR (50) NOT NULL,
+    [charEyeColor]  NVARCHAR (50) NOT NULL,
+    [charTattoo]    BIT           NOT NULL,
+    
+);
+
